4a96a94388e0e6758b570a4eb5c25fb9a0f88571
Ian_ Kao <iankao1005@hotmail.com>
2019-05-18 21:58:39 +0800

    自訂手機輸入驗證屬性

--- MVC5HWWeek1/Models/客戶聯絡人.Partial.cs
+++ MVC5HWWeek1/Models/客戶聯絡人.Partial.cs
@@ -54,6 +54,7 @@ namespace MVC5HWWeek1.Models
         public string Email { get; set; }
         
         [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        [RegularExpression(@"\d{4}-\d{6}", ErrorMessage = "格式錯誤(e.g.0911-111111)")]
         public string 手機 { get; set; }
         
         [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
